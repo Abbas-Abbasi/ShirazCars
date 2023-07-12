5e867cb546be78beb516f9e96b96010535a3df68
Abbas Abbasi <137259478+Abbas-Abbasi@users.noreply.github.com>
2023-07-12 11:20:32 +0330

    ShirazCars

---
+++ main/filters.py
@@ -0,0 +1,10 @@
+import django_filters
+
+from .models import Listing
+
+
+class ListingFilter(django_filters.FilterSet):
+    class Meta:
+        model = Listing
+        fields = {'transmission': ['exact'], 'brand': [
+            'exact'], 'model': ['contains'], 'mileage': ['lt']}
